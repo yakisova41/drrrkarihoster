e678bf104578d2698243fced260aa6c577b010e3
yakisova41 <yakisoba5emon@gmail.com>
2022-07-01 17:06:13 +0000

    add config example

---
+++ config.py
@@ -0,0 +1,5 @@
+config = {
+    "name":"名無し",
+    "icon":"bm",
+    "useragent":"Mozilla/5.0 (iphone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/102.0.5005.87 Mobile/15E148 Safari/604.1"
+}
